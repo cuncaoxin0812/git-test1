0b8742746a374c4ca741da76d7935321c0b26fcd
hugh <670567211@qq.com>
2022-02-07 16:28:23 +0800

    idea master third commit

--- src/com/hugh/gitTest.java
+++ src/com/hugh/gitTest.java
@@ -5,6 +5,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("bbbb");
+        System.out.println("master");
 
     }
 
